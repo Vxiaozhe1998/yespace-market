2800eb845d08f389def43aa1766a4aa6f7f339ea
傅文雅 <1343396826@qq.com>
2019-08-18 21:24:46 +0800

    1.新增界面 searchzuixin.html(搜索结果为带筛选框的电脑) 2.修改DN SX DNZK 的最高价最低价 3.（缺漏：拖动条未完成，轮播界面未完成）

--- asserts/js/Yespacejs.js
+++ asserts/js/Yespacejs.js
@@ -131,3 +131,47 @@ function liuup(elmnt){
 // 	var yyliu = document.getElementById('yuyin');
 // 	elmnt.style.display= "clr";
 // }
+
+
+// 滑动块
+
+// $(document).ready(function(){
+//  var tag = false,dx = 0,left = 0,bgleft = 0;
+//  $('.progress_btn').mousedown(function(e) {
+//   dx = e.pageX - left;
+//   tag = true;
+//  });
+//  $(document).mouseup(function(e) {
+//   tag = false;
+//  });
+//  $('.progress_btn').mousemove(function(e) {//鼠标移动
+//   if (tag) {
+//    left = e.pageX - dx;
+//    if (left <= 0) {
+//     left = 0;
+//    }else if (left > 300) {
+//     left = 300;
+//    }
+//    $('.progress_btn').css('left', left);
+//    $('.progress_bar').animate({width:left},1);
+//    $('.text').html(parseInt((left/300)*100) + '%');
+//   }
+//  });
+//  $('.progress_bg').click(function(e) {//鼠标点击
+//   if (!tag) {
+//    bgleft = $('.progress_bg').offset().left;
+//    left = e.pageX - bgleft;
+//    if (left <= 0) {
+// left=0;
+//    }else if (left > 300) {
+//     left = 300;
+//    }
+//    $('.progress_btn').css('left', left);
+//    $('.progress_bar').animate({width:left},300);//
+//    $('.text').html(parseInt((left/300)*100) + '%');
+//   }
+// //  alert(left);
+//  });
+//  
+// });
+// 
